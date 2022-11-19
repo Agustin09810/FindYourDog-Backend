9ce1516d165a4b1fe79e5ea1eb6d12f5e2eac3e1
tomasrama12 <88690021+tomasrama12@users.noreply.github.com>
2022-11-19 18:23:02 -0300

    chatid

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //router.get("/:id", usersController.getUserById);
 router.get("/", usersController.getUser);
 router.get("/:username", usersController.getUserByUsername);
-router.put("/", usersController.updateUserByUsername);
+router.put("/:username", usersController.updateUserByUsername);
 
 
 
